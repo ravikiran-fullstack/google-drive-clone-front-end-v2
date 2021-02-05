0347702687150251f503d20382de51ae1cb258ad
kiran-code <ravikiransjce.code@gmail.com>
2021-02-05 13:34:45 +0530

    Added @aws-sdk/client-s3 to upload files to S3, earlier I was using react-aws-s3

--- src/components/upload/Upload.js
+++ src/components/upload/Upload.js
@@ -1,38 +1,75 @@
 import React, { useRef } from "react";
 
-import S3 from 'react-aws-s3';
+// import S3 from 'react-aws-s3';
+import {
+  S3Client,
+  ListBucketsCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
 
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 
+import { nanoid } from "nanoid";
+
 import useStyles from "./styles";
 
 const Upload = () => {
   const fileInput = useRef();
   const classes = useStyles();
+  const config = {
+    bucketName: process.env.REACT_APP_BUCKET_NAME,
+    dirName: process.env.REACT_APP_DIR_NAME /* optional */,
+    region: process.env.REACT_APP_REGION,
+    accessKeyId: process.env.REACT_APP_ACCESS_ID,
+    secretAccessKey: process.env.REACT_APP_ACCESS_KEY,
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     let file = fileInput.current.files[0];
     let newFileName = fileInput.current.files[0].name.replace(/\..+$/, "");
-    const config = {
-      bucketName: process.env.REACT_APP_BUCKET_NAME,
-      dirName: process.env.REACT_APP_DIR_NAME /* optional */,
-      region: process.env.REACT_APP_REGION,
-      accessKeyId: process.env.REACT_APP_ACCESS_ID,
-      secretAccessKey: process.env.REACT_APP_ACCESS_KEY,
+    const randomName = nanoid(10).toLowerCase();
+    const uploadParams = {
+      Bucket: config.bucketName,
+      // Specify the name of the new object. For example, 'index.html'.
+      // To create a directory for the object, use '/'. For example, 'myApp/package.json'.
+      Key: `google-drive-clone/${randomName}.jpg`,
+      // Content of the new object.
+      Body: file,
+      ACL:'public-read'
     };
-    
-    const ReactS3Client = new S3(config);
-    ReactS3Client.uploadFile(file, newFileName).then((data) => {
-      console.log(data);
-      if (data.status === 204) {
-        console.log("success");
-      } else {
-        console.log("fail");
-      }
+    const bucketParams = { Bucket: config.bucketName }; 
+    const creds = {
+      accessKeyId: config.accessKeyId,
+      secretAccessKey: config.secretAccessKey,
+    };
+
+    const s3 = new S3Client({
+      apiVersion: "2006-03-01",
+      region: config.region,
+      credentials: creds,
     });
+    let str = `https://${config.bucketName}.s3-${config.region}.amazonaws.com/google-drive-clone/${randomName}.jpg`
+    console.log('str ',str);
+    s3.send(new PutObjectCommand(uploadParams))
+      .then((data) => console.log(data, str))
+      .catch((err) => console.log(err));
+
+    s3.send(new ListBucketsCommand(bucketParams))
+      .then((data) => console.log('ListBucketsCommand ',data))
+      .catch((err) => console.log(err));
+
+    // const ReactS3Client = new S3(config);
+    // ReactS3Client.uploadFile(file, newFileName).then((data) => {
+    //   console.log(data);
+    //   if (data.status === 204) {
+    //     console.log("success");
+    //   } else {
+    //     console.log("fail");
+    //   }
+    // });
   };
   return (
     <>
